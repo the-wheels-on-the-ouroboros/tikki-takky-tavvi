074de46ed688eefdc3ae00de7bbdcd0b30f0695d
Spencer <scarvill91@gmail.com>
2016-03-15 14:50:14 -0500

    Consolidate Tests file into TestRunner.elm

--- TestRunner.elm
+++ TestRunner.elm
@@ -2,14 +2,23 @@ module Main where
 
 import Signal exposing (Signal)
 
-import ElmTest exposing (consoleRunner)
+import ElmTest exposing (..)
 import Console exposing (IO, run)
 import Task
 
-import Tests
+import ModelTests
+import UpdateTests
+
+tests : Test
+tests =
+  suite "Tic Tac Toe tests"
+    [
+      ModelTests.all,
+      UpdateTests.all
+    ]
 
 console : IO ()
-console = consoleRunner Tests.all
+console = consoleRunner tests
 
 port runner : Signal (Task.Task x ())
 port runner = run console
