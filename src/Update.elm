917ef8cd97bf978c2dfbd09c9bc0b765e2934742
Spencer <scarvill91@gmail.com>
2016-03-15 20:59:44 -0500

    Add move verification to game model
      * makeMove will not change the game state when a move has already
        been made in the same location.

--- src/Update.elm
+++ src/Update.elm
@@ -1,10 +1,13 @@
 module Update where
 
-import Model exposing (GameState, Coordinates, Move, other)
+import Model exposing (GameState, Coordinates, Move, other, playerWhoMovedAt)
 
 makeMove : Coordinates -> GameState -> GameState
-makeMove coordinates state =
-  { state |
-    currentPlayer = other state.currentPlayer,
-    movesSoFar = Move coordinates state.currentPlayer :: state.movesSoFar
-  }
+makeMove coordinates gameState =
+  case playerWhoMovedAt coordinates gameState of
+    Just _ -> gameState
+    Nothing -> {
+        gameState |
+          currentPlayer = other gameState.currentPlayer,
+          movesSoFar = Move coordinates gameState.currentPlayer :: gameState.movesSoFar
+      }
--- tests/UpdateTests.elm
+++ tests/UpdateTests.elm
@@ -12,18 +12,31 @@ all =
       suite "Making a move" <|
         let
           player = X
-          gameState = GameState 3 player []
-          coordinates = Coordinates 0 0
+          targetCoordinates = (Coordinates 0 0)
         in
           [
-            test "Records the move made in a new game state" <|
-              assertEqual
-                [ Move coordinates player ]
-                (makeMove coordinates gameState).movesSoFar,
+            suite "When a move has not been made at target coordinates" <|
+              let
+                gameState = GameState 3 player []
+              in
+                [
+                  test "Records the move made in a new game state" <|
+                    assertEqual
+                      [ Move targetCoordinates player ]
+                      (makeMove targetCoordinates gameState).movesSoFar,
 
-            test "Returns a game state in which the other player is now the current player" <|
-              assertEqual
-                (Model.other player)
-                (makeMove coordinates gameState).currentPlayer
+                  test "Returns a game state in which the other player is now the current player" <|
+                    assertEqual
+                      (Model.other player)
+                      (makeMove targetCoordinates gameState).currentPlayer
+                ],
+            suite "When a move has already been made at target coordinates" <|
+              let
+                gameState = GameState 3 (Model.other player) [ Move targetCoordinates player ]
+              in
+                [
+                  test "Returns an unchanged game state" <|
+                    assertEqual gameState <| makeMove targetCoordinates gameState
+                ]
           ]
     ]
