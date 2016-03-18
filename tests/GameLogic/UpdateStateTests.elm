a6f8f9fed17a067dee5de13dd9514fa6c608ca99
Spencer <scarvill91@gmail.com>
2016-03-18 10:35:58 -0500

    Edit UpdateState test descriptions

--- tests/GameLogic/UpdateStateTests.elm
+++ tests/GameLogic/UpdateStateTests.elm
@@ -1,6 +1,6 @@
 module GameLogic.UpdateStateTests where
 
-import ElmTest exposing (Test, assertEqual, suite, test)
+import ElmTest exposing (Test, assert, assertEqual, suite, test)
 
 import GameModel exposing (Coordinates, GameState, Move, Player (X, O), Status (InProgress, Tied, Won))
 import TestHelpers exposing (x, o)
@@ -14,7 +14,7 @@ all =
 
         [ suite "Making a move"
 
-            [ test "Records the move made in a new game state" <|
+            [ test "Returns a game state in which a move has been made at given coordinates" <|
                 assertEqual [ Move (Coordinates 0 0) X ]
                     <| .movesSoFar
                     <| makeMove (Coordinates 0 0)
@@ -26,19 +26,19 @@ all =
                     <| makeMove (Coordinates 0 0)
                     <| GameState 3 X [] InProgress
 
-            , test "Returns an unchanged game state when a move already exists at coordinates" <|
+            , test "Does not make move if a move already exists at coordinates" <|
                 let
                     gameState = GameState 3 (otherPlayer X) [ Move (Coordinates 0 0) X ] InProgress
                 in
                     assertEqual gameState (makeMove (Coordinates 0 0) gameState)
 
-            , test "Returns an unchanged game state when given game state status is Tied" <|
+            , test "Does not make move if given game state status is Tied" <|
                 let
                     gameState = GameState 3 X [] Tied
                 in
                     assertEqual gameState (makeMove (Coordinates 0 0) gameState)
 
-            , test "Returns an unchanged game state when given game state status is Won" <|
+            , test "Does not make move if given game state status is Won" <|
                 let
                     gameState = GameState 3 X [] (Won X)
                 in
@@ -46,13 +46,13 @@ all =
 
             , suite "Updating the game status"
 
-                [ test "Game state has status InProgress if game is not over after move is made" <|
+                [ test "Game has status InProgress if game is not over after move is made" <|
                     assertEqual InProgress
                         <| .status
                         <| makeMove (Coordinates 0 0)
                         <| GameState 3 X [] InProgress
 
-                , test "Game state has status Tied if game is tied after move is made" <|
+                , test "Game has status Tied if game is tied after move is made" <|
                     let
                         moves =
                             [ x 0 0, x 0 1, o 0 2
@@ -65,7 +65,7 @@ all =
                             <| makeMove (Coordinates 2 2)
                             <| GameState 3 X moves InProgress
 
-                , test "Returns a game state with status Won X if player X won the game" <|
+                , test "Game has status Won X if player X won the game" <|
                     let
                         moves =
                             [ x 0 0, x 0 1
@@ -77,7 +77,7 @@ all =
                             <| makeMove (Coordinates 0 2)
                             <| GameState 3 X moves InProgress
 
-                , test "Returns a game state with status Won O if player O won the game" <|
+                , test "Game has status Won O if player O won the game" <|
                     let
                         moves =
                             [ o 0 0, o 0 1
